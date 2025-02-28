bfc38bb10423bbc3d237a71274cb646c3a7cac27
abhaseeb200 <haseebabdul747@gmail.com>
2025-02-28 18:25:04 +0500

    Accept className for flexible styling

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,13 +1,13 @@
-const Card = ({ children }) => {
+const Card = ({ className, children }) => {
   return (
-    <div className="bg-white drop-shadow-md rounded-md mb-3 dark:bg-dark-200">
+    <div className={`${className} bg-white drop-shadow-md rounded-md mb-3 dark:bg-dark-200`}>
       {children}
     </div>
   );
 };
 
 const CardHeading = ({title}) => {
-  return <div className="text-lg text-gray-600 dark:text-gray-200 font-medium py-6 px-5">{title}</div>;
+  return <div className="text-lg text-gray-600 dark:text-gray-200 font-medium">{title}</div>;
 };
 
 export { Card, CardHeading };
