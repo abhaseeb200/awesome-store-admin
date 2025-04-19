99f6c6f8b6dd7a46ca84e8d240cb65f15941ffd8
abhaseeb200 <haseebabdul747@gmail.com>
2025-04-19 23:21:41 +0500

    Add the dynamic classname and add w-full in the main wrapper dom

--- src/components/inputs/index.jsx
+++ src/components/inputs/index.jsx
@@ -1,6 +1,6 @@
 const Input = ({ isError, messageError, ...props }) => {
   return (
-    <div>
+    <div className="w-full">
       <label className="capitalize text-sm leading-none text-gray-600 dark:text-gray-300">
         {props?.label}
       </label>
@@ -8,11 +8,11 @@ const Input = ({ isError, messageError, ...props }) => {
         {...props}
         className={`${
           isError ? `border-red-500` : `border-gray-300 focus:border-primary`
-        } border bg-white dark:bg-dark-200 text-gray-800 dark:text-gray-200 rounded-md text-sm leading-none py-2.5 w-full px-3 placeholder:text-md outline-0 focus:drop-shadow-md transition`}
+        } ${props?.className} border bg-white dark:bg-dark-200 text-gray-800 dark:text-gray-200 rounded-md text-sm leading-none py-2.5 w-full px-3 placeholder:text-md outline-0 focus:drop-shadow-md transition`}
       />
-      {isError ? (
+      {isError > 0 && (
         <small className="text-red-500 block">{messageError}</small>
-      ): ""}
+      )}
     </div>
   );
 };
