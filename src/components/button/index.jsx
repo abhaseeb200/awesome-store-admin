9ba164ad05a0cba9d9a96d446ab3119c17c93c90
abhaseeb200 <haseebabdul747@gmail.com>
2025-04-19 23:21:01 +0500

    Remove the cursor-pointer class

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -14,7 +14,7 @@ const Button = ({
       {...props}
       type={type}
       disabled={isLoading}
-      className={`cursor-pointer text-sm bg-primary hover:bg-primaryDark rounded-md drop-shadow-md text-white text-md px-5 py-2 transition flex items-center gap-x-2 ${className}`}
+      className={`text-sm bg-primary hover:bg-primaryDark rounded-md drop-shadow-md text-white text-md px-5 py-2 transition flex items-center gap-x-2 ${className}`}
     >
       {isLoading ? <CgSpinner className="animate-spin size-5" /> : null}
       {icon} {name}
