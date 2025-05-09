b8c3ac7b5a6ecf0e156ab3f02abf80020a9209c2
abhaseeb200 <haseebabdul747@gmail.com>
2025-05-10 01:37:08 +0500

    Remove the background shadow

--- src/components/card/skeleton.jsx
+++ src/components/card/skeleton.jsx
@@ -1,6 +1,6 @@
 const CardSkeleton = () => {
   return (
-    <div className="bg-white dark:bg-dark-200 drop-shadow-md rounded-md px-5 py-8 mb-3 animate-pulse">
+    <div className="bg-white dark:bg-dark-200 rounded-md px-5 py-8 mb-3 animate-pulse">
       <div className="p-5 bg-gray-300 dark:bg-dark-600"></div>
       <div className="p-5 my-3 bg-gray-300 dark:bg-dark-600"></div>
       <div className="p-5 bg-gray-300 dark:bg-dark-600"></div>
