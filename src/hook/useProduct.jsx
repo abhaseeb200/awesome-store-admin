1461aad3346be943cd6feb5fd885e3d2b1f76472
Abdul Haseeb <haseebabdul747@gmail.com>
2025-06-17 13:05:14 +0500

    Add select-brand functionality and Add a product intragration

--- src/hook/useProduct.jsx
+++ src/hook/useProduct.jsx
@@ -4,17 +4,15 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import useDebounce from "@/hook/useDebounce";
 
-function useBrand(isSelectQuery) {
+function useProduct() {
   const queryClient = useQueryClient();
   const [sorting, setSorting] = useState([{}]);
   const [searchValue, setSearchValue] = useState("");
-  const [brandsSelect, setBrandsSelect] = useState([]);
   const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 10 });
   const debouncedSearch = useDebounce(searchValue, 500);
 
-  const brandsFetch = useQuery({
-    queryKey: ["brands", sorting, debouncedSearch, pagination],
-    enabled: !isSelectQuery, // USE CASE: IF FALSE NOT FETCH API, DEFAULT => TRUE
+  const productFetch = useQuery({
+    queryKey: ["products", sorting, debouncedSearch, pagination],
     queryFn: async () => {
       try {
         const sort = sorting[0];
@@ -24,7 +22,7 @@ function useBrand(isSelectQuery) {
           : "";
 
         const response = await API.get(
-          `brands?page=${
+          `products?page=${
             pagination?.pageIndex + 1
           }${sortBy}${sortOrder}&q=${debouncedSearch}`
         );
@@ -35,31 +33,11 @@ function useBrand(isSelectQuery) {
     },
   });
 
-  const selectQuery = useQuery({
-  queryKey: ["brands-select", pagination, debouncedSearch],
-  enabled: isSelectQuery, 
-  queryFn: async () => {
-    const response = await API.get(`brands?page=${pagination?.pageIndex + 1}&q=${debouncedSearch}`);
-    let responseData = response?.data?.data;
-    let updated = [];
-    
-    if (searchValue) {
-      updated = [...responseData];
-    } else {
-      updated = pagination?.pageIndex == 0 ? [...responseData] : [...brandsSelect,...responseData]
-    }
-
-    setBrandsSelect(updated)
-    return response?.data || [];
-  },
-  })
-
   const createMutation = useMutation({
     mutationFn: (data) => {
-      return API.post("/brands/add", data);
+      return API.post("/products/add", data);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["brands"] });
       toast.success("Added successfully");
     },
     onError: (error) => {
@@ -69,10 +47,10 @@ function useBrand(isSelectQuery) {
 
   const updateMutation = useMutation({
     mutationFn: (data) => {
-      return API.put(`/brands/update/${data?.id}`, data?.value);
+      return API.put(`/products/update/${data?.id}`, data?.value);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["brands"] });
+      queryClient.invalidateQueries({ queryKey: ["products"] });
       toast.success("Updated successfully");
     },
     onError: (error) => {
@@ -82,10 +60,10 @@ function useBrand(isSelectQuery) {
 
   const deleteMutation = useMutation({
     mutationFn: async (data) => {
-      await API.delete(`brands/delete/${data?._id}`);
+      await API.delete(`products/delete/${data?._id}`);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["brands"] });
+      queryClient.invalidateQueries({ queryKey: ["products"] });
       toast.success("Delete successfully");
     },
     onError: () => {
@@ -94,19 +72,17 @@ function useBrand(isSelectQuery) {
   });
 
   return {
-    brandsFetch,
+    productFetch,
     createMutation,
     updateMutation,
     deleteMutation,
     sorting,
     searchValue,
     pagination,
-    selectQuery,
-    brandsSelect,
     setSorting,
     setSearchValue,
     setPagination,
   };
 }
 
-export default useBrand;
+export default useProduct;
