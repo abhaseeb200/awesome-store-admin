50495bc109831bed00faaacb9b85e6b42466ec2a
Abdul Haseeb <haseebabdul747@gmail.com>
2025-06-17 13:03:56 +0500

    handling error message

--- src/screens/login/index.jsx
+++ src/screens/login/index.jsx
@@ -41,7 +41,7 @@ const Login = () => {
       navigate("/dashboard");
     },
     onError: (error) => {
-      toast.error(error?.message || "Something went wrong");
+      toast.error(error?.response?.data?.message || error?.message || "Something went wrong");
     },
   });
 
